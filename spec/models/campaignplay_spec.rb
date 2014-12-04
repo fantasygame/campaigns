e7c3441e5723f42a0e62145ef999fa41f8d471bf
Jakub Kubacki <kubacki.jk@gmail.com>
2014-12-04 23:14:50 +0100

    Join tables specs

--- spec/models/campaignplay_spec.rb
+++ spec/models/campaignplay_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Campaignplay, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:campaign) }
+  it { is_expected.to belong_to(:user) }
 end
