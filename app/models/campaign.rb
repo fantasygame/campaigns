41c5f06f1d6d1dd78a584286bc7cc8f811b72a11
Jakub Kubacki <kubacki.jk@gmail.com>
2014-12-09 23:31:50 +0100

    Game show

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -4,7 +4,7 @@ class Campaign < ActiveRecord::Base
   belongs_to :game_master, class_name: 'User'
   has_many :users, through: :campaignplays
   has_many :campaignplays
-  has_many :games, dependent: :destroy
+  has_many :games, -> { order(date: :desc) }, dependent: :destroy
 
   validates :name, presence: :true
   validates :game_master, presence: :true
