4edaa496862a72fc5f1dc19d3dbabeb3b99accf5
Jakub Kubacki <kubacki.jk@gmail.com>
2014-12-09 23:17:10 +0100

    Game form and controller with decent_exposure

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,48 +1,39 @@
 class GamesController < ApplicationController
-  before_action :set_game, only: [:show, :edit, :update, :destroy]
+  expose(:campaign)
+  expose(:games) { campaign.games }
+  expose(:game, attributes: :game_params)
 
-  respond_to :html
-
-  def index
-    @games = Game.all
-    respond_with(@games)
-  end
+  respond_to :html, :json, :xml
 
   def show
-    respond_with(@game)
+    respond_with(game)
   end
 
   def new
-    @campaign = Campaign.find(params[:campaign_id])
-    @game = Game.new
-    respond_with(@game)
+    respond_with(game)
   end
 
   def edit
   end
 
   def create
-    @game = Game.new(game_params)
-    @game.save
-    respond_with(@game)
+    game.save
+    respond_with(campaign)
   end
 
   def update
-    @game.update(game_params)
-    respond_with(@game)
+    game.update(game_params)
+    respond_with(game)
   end
 
   def destroy
-    @game.destroy
-    respond_with(@game)
+    game.destroy
+    respond_with(game)
   end
 
   private
-    def set_game
-      @game = Game.find(params[:id])
-    end
 
     def game_params
-      params.require(:game).permit(:name, :campaign_id)
+      params.require(:game).permit(:name, :date)
     end
 end
