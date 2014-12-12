37547fb920497c761b450dbb0d8c36e71f7fa67e
Jakub Kubacki <kubacki.jk@gmail.com>
2014-12-12 23:14:38 +0100

    Add post specs.

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Game, :type => :model do
+RSpec.describe Game, type: :model do
   
 	it { is_expected.to belong_to(:campaign) }
 	it { is_expected.to have_many(:users) }
+  it { is_expected.to have_many(:posts) }
+  
 	it { is_expected.to validate_presence_of(:campaign) }
 	it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:date) }
