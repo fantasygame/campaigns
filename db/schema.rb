c9b96d7177d95c1069d7dd3e000a75f3349a82af
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-05-27 13:11:07 +0200

    Add field description to items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150525172437) do
+ActiveRecord::Schema.define(version: 20150527105705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 20150525172437) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.integer  "sold_price",  default: 0
+    t.text     "description"
   end
 
   add_index "items", ["campaign_id"], name: "index_items_on_campaign_id", using: :btree
