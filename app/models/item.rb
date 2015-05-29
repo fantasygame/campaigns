2046b7834d630d41b1009d65a23b303923cd8b40
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-05-29 23:29:55 +0200

    Add specs for reclaim_sell service

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,5 @@ class Item < ActiveRecord::Base
   belongs_to :campaign
   belongs_to :hero
 
-  belongs_to :last_owner, class_name: 'User'
+  belongs_to :last_owner, class_name: 'Hero'
 end
---
+++ spec/services/reclaim_item_spec.rb
@@ -0,0 +1,24 @@
+# require 'rails_helper'
+
+# RSpec.describe ReclaimItem, type: :class do
+#   let(:campaign) { create(:campaign, money: 100) }
+#   let(:item) { create(:item, sold: true, sold_price: 100, hero_id: nil, last_owner_id: 2) }
+
+#   before { described_class.new(item, campaign).call }
+
+#   it 'removes sold price from campaign money' do
+#     expect(campaign.money).to eq 0
+#   end
+
+#   it 'sets sold price back to 0' do
+#     expect(item.sold_price).to eq 0
+#   end
+
+#   it 'sets sold to false' do
+#     expect(item.sold).to eq false
+#   end
+
+#   it 'sets last owner as owner' do
+#     expect(item.hero_id).to eq 2
+#   end
+# end
