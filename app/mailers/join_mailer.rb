5e1e023d004ba17eed8ae7b1682cb22b977aa88b
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-05-30 13:41:05 +0200

    Create Join mailer to send request to join campaign

---
+++ app/mailers/join_mailer.rb
@@ -0,0 +1,8 @@
+class JoinMailer < ApplicationMailer
+  def join_request(user, campaign)
+    @game_master = campaign.game_master
+    @user = user
+    @campaign = campaign
+    mail(to: @game_master.email, subject: "There is new request from #{@user.name} to join #{@campaign.name} campaign.")
+  end
+end
