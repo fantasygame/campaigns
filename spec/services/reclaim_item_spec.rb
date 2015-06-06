32f16adf852664549b0c3ef20e2e60340847954c
Jakub Kubacki <kubacki.jk@gmail.com>
2015-06-06 15:02:21 +0200

    Fix specs due to hero item selling/reclaiming

--- spec/services/reclaim_item_spec.rb
+++ spec/services/reclaim_item_spec.rb
@@ -2,14 +2,10 @@ require 'rails_helper'
 
 RSpec.describe ReclaimItem, type: :class do
   let(:campaign) { create(:campaign, money: 100) }
-  let(:hero) { create(:hero, id: 2) }
+  let(:hero) { create(:hero, id: 2, money: 200) }
   let(:item) { create(:item, sold: true, sold_price: 100, hero: hero) }
 
   before { described_class.new(item, campaign).call }
-
-  it 'removes sold price from campaign money' do
-    expect(campaign.money).to eq 0
-  end
 
   it 'sets sold price back to 0' do
     expect(item.sold_price).to eq 0
@@ -17,5 +13,18 @@ RSpec.describe ReclaimItem, type: :class do
 
   it 'sets sold to false' do
     expect(item.sold).to eq false
+  end
+
+  context "item belongs to hero" do
+    it 'removes sold price from hero money' do
+      expect(hero.money).to eq 100
+    end
+  end
+
+  context "item has no hero" do
+    let(:item) { create(:item, campaign_id: campaign, sold_price: 100) }
+    it 'removes sold price from campaign money' do
+      expect(campaign.money).to eq 0
+    end
   end
 end
--- spec/services/sell_item_spec.rb
+++ spec/services/sell_item_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe SellItem, type: :class do
   let(:campaign) { create(:campaign, money: 0) }
-  let(:hero) { create(:hero, id: 2) }
-  let(:item) { create(:item, campaign_id: campaign, price: 100, hero_id: hero.id) }
+  let(:hero) { create(:hero, id: 2, money: 0) }
+  let(:item) { create(:item, campaign: campaign, price: 100, hero: hero) }
 
   before { described_class.new(item, campaign).call }
 
@@ -11,11 +11,20 @@ RSpec.describe SellItem, type: :class do
     expect(item.sold_price).to eq 50
   end
 
-  it 'adds sold item price to campaign money' do
-    expect(campaign.money).to eq 50
-  end
-
   it 'marks item as sold' do
     expect(item.sold).to eq true
+  end
+
+  context "item belongs to hero" do
+    it 'adds sold item price to hero money' do
+      expect(hero.money).to eq 50
+    end
+  end
+
+  context "item has no hero" do
+    let(:item) { create(:item, campaign_id: campaign, price: 100) }
+    it 'adds sold item price to campaign money' do
+      expect(campaign.money).to eq 50
+    end
   end
 end
