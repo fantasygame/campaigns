ed029707561e447b328a5e5bef9b1768bddb8bbf
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-07-09 16:21:24 +0200

    Idea index view with buttons to edit, show, add and destroy

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,12 +1,20 @@
 class IdeasController < ApplicationController
-  expose(:idea)
-  expose(:ideas)
+  expose(:idea, attributes: :idea_params)
+  expose(:ideas) { campaign.ideas }
   expose(:campaign)
+  expose(:campaigns)
   expose(:user)
   before_action :authenticate_user!
 
   def index
-    authorize idea
   end
 
+  def show
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:title, :description, :id, :campaign, :user)
+  end
 end
