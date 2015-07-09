d0137addeb54df7b310d56a8cb409f9a39257979
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-07-09 15:02:35 +0200

    Desent expose Idea controller

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,3 +1,12 @@
 class IdeasController < ApplicationController
+  expose(:idea)
+  expose(:ideas)
+  expose(:campaign)
+  expose(:user)
+  before_action :authenticate_user!
+
+  def index
+    authorize idea
+  end
 
 end
