6de071536a964ba9227dc970bf28fd65c38d8383
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-07-10 15:01:26 +0200

    Validates presence of title, campaign and user in Idea

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,8 @@
 class Idea < ActiveRecord::Base
   belongs_to :user
   belongs_to :campaign
+
+  validates :title, presence: true
+  validates :campaign, presence: true
+  validates :user, presence: true
 end
