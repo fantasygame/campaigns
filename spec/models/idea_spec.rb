2645b467d63acba6f2aee4f4eeddd36a4ae2faac
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-07-14 14:52:18 +0200

    Specs for validation in Idea model

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Idea, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Idea, type: :model do
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:campaign) }
+  it { is_expected.to validate_presence_of(:user) }
 end
