29487c72837b88eb99f0c9cce6743be14a28da52
Małgorzata Grobelska <grobelska.m@gmail.com>
2015-07-22 13:31:54 +0200

    Specs for PoinCalculator service

--- spec/services/points_calculator_spec.rb
+++ spec/services/points_calculator_spec.rb
@@ -1,40 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe PointsCalculator, type: :class do
+  let(:campaign) { create(:campaign, users: [user]) }
+  let(:game) { create(:game, campaign: campaign) }
   let(:user) { create(:user) }
-  let(:user2) { create(:user) }
-  let(:campaign) { create(:campaign, users: [user, user2]) }
-  let(:service) { described_class.new(user, campaign) }
 
-  before { service.call }
+  before(:each) { service.call }
 
   describe "there is no campaign" do
-    let(:campaign2) { create(:campaign, users: [user], games: [game]) }
-    let(:game) { create(:game, campaign: campaign2) }
-    let(:post) { create(:post, user: user, content: 'cabaic', game: game) }
-    let(:post2) { create(:post, user: user, content: 'cebaic', game: game) }
+    let!(:post) { create(:post, user: user, content: 'a') }
     let(:service) { described_class.new(user) }
 
     it 'sets default campaign to nil' do
       expect(service.campaign).to eq nil
     end
-    # it 'counts all user posts' do
-    #   expect(service.call).to eq 2
-    # end
-  end
-  describe "campaign is set" do
-    context "user is campaign member" do
-      let(:game) { create(:game, campaign: campaign) }
-      let(:user_post) { create(:post, game: game) }
-      let(:user2_post) { create(:post, game: game) }
-      # it 'returns Fixnum value' do
-      #   expect(service.call).to be_a(Fixnum)
-      # end
-      # it 'returns only users posts' do
-      #   expect(service.call).to eq 1
-      # end
+    it 'counts all user posts' do
+      expect(service.call).to eq 1
     end
-    context "user is not campaign member" do
+    describe 'user have vote' do
+      let!(:vote) { create(:vote, user: user, post: post) }
+      it 'sums post and vote and returns 2' do
+        expect(service.call).to eq 2
+      end
+    end
+    describe 'user have idea' do
+      let!(:idea) { create(:idea, user: user, campaign: campaign) }
+      context 'idea is not marked as interesting' do
+        it 'counts only post' do
+          expect(service.call).to eq 1
+        end
+      end
+      context 'idea is marked as interesting' do
+        let!(:idea) { create(:idea, user: user, campaign: campaign, interesting: true) }
+        describe 'idea is not used' do
+          it 'counts idea with post and returns 2' do
+            expect(service.call).to eq 2
+          end
+        end
+        describe 'idea is used' do
+          let!(:idea) do
+            create(:idea, user: user, campaign: campaign, interesting: true, used: true)
+          end
+          it 'adds additional point for used idea and returns 3' do
+            expect(service.call).to eq 3
+          end
+        end
+      end
+    end
+  end
+
+  describe "campaign is set" do
+    let(:campaign2) { create(:campaign, users: [user]) }
+    let(:game2) { create(:game, campaign: campaign2) }
+    let!(:post2) { create(:post, user: user, content: 'a', game: game2) }
+    let!(:post) { create(:post, user: user, content: 'a', game: game) }
+    let!(:idea) do
+      create(:idea, user: user, campaign: campaign, interesting: true, used: true)
+    end
+    let(:service) { described_class.new(user, campaign) }
+
+    it 'sums post with used idea (2 points) from campaign and returns 3' do
+      expect(service.call).to eq 3
     end
   end
 end
