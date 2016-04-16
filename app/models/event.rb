b6d02ea1b8c2d08d30108ae2228c7c48a8b6694e
Jakub Kubacki <kubacki.jk@gmail.com>
2016-04-16 15:42:47 +0200

    Add event #date_options
    Fixed dates in mvp

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,16 @@ class Event < ActiveRecord::Base
     campaign.events.find_index(self) + 1
   end
 
+  def date_options
+    %w(mon tue wed thu fri sat sun).each_with_object([]) do |day, options|
+      options << option_hash(day, 10) if %w(sat sun).include?(day)
+      options << option_hash(day, 18)
+    end
+  end
+
+  private
+
+  def option_hash(day, hour)
+    { day: day, hour: hour }
+  end
 end
