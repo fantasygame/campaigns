46d17c2967831d213d93e983eda6f8a948a02078
Jakub Kubacki <kubacki.jk@gmail.com>
2016-04-16 19:22:52 +0200

    Event name

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -2,4 +2,7 @@ class EventSerializer < ActiveModel::Serializer
   attributes :id, :date, :name
   has_many :event_options
 
+  def name
+    "#{object.campaign.name} #{object.event_number}"
+  end
 end
