03d37b970f17608aa2a367f5154601d6c55a9d8a
Jakub Kubacki <kubacki.jk@gmail.com>
2016-04-16 15:52:40 +0200

    Add temp date to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,10 @@ class Event < ActiveRecord::Base
     campaign.events.find_index(self) + 1
   end
 
+  def date
+    DateTime.now.strftime("%d-%m-%Y at %H:00")
+  end
+
   def date_options
     %w(mon tue wed thu fri sat sun).each_with_object([]) do |day, options|
       options << option_hash(day, 10) if %w(sat sun).include?(day)
