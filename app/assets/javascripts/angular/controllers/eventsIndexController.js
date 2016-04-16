d23611b9a972ff2db118d147a180bbb99e25f351
Jakub Kubacki <kubacki.jk@gmail.com>
2016-04-16 20:36:38 +0200

    Add classes for selected options

--- app/assets/javascripts/angular/controllers/eventsIndexController.js
+++ app/assets/javascripts/angular/controllers/eventsIndexController.js
@@ -1,3 +1,13 @@
 angular.module("EventsApp").controller('EventsIndexController', function(Event, $scope){
   $scope.events = Event.query();
+
+  $scope.selectClass = function(option) {
+    var classes = ["option-button", "btn", "btn-sm"];
+    if (option.selected) {
+      classes.push("btn-success");
+    } else {
+      classes.push("btn-default");
+    }
+    return classes.join(" ");
+  };
 });
