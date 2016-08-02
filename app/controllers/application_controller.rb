9dabb80b404b188e196427c29c6518ad3ad1a559
Jakub Kubacki <kubacki.jk@gmail.com>
2016-08-02 17:57:06 +0200

    Remove decent exposure configuration
    It is now default

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  decent_configuration do
-    strategy DecentExposure::StrongParametersStrategy
-  end
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
