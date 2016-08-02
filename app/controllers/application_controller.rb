f792a371ed2a46772b8cd6d3d0c1c2db1f2203f4
Jakub Kubacki <kubacki.jk@gmail.com>
2016-08-02 17:56:50 +0200

    Update sanitizer options to new devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,11 +10,15 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
   private
