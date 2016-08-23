077344f2765ba56ca637774ba7098f5f9930260a
Jakub Kubacki <kubacki.jk@gmail.com>
2016-08-23 17:45:17 +0200

    false config.active_record.belongs_to_required_by_default

--- config/application.rb
+++ config/application.rb
@@ -37,5 +37,7 @@ module Campaigns
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
+
+    config.active_record.belongs_to_required_by_default = false
   end
 end
